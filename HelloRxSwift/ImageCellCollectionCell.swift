008b555793fb3a24acff30b081ba6b69089602d4
Ezequiel Barreto <>
2020-10-02 15:54:09 -0500

    photo permissions

---
+++ HelloRxSwift/ImageCellCollectionCell.swift
@@ -0,0 +1,44 @@
+//
+//  ImageCellCollectionCell.swift
+//  HelloRxSwift
+//
+//  Created by Ezequiel Barreto on 02/10/20.
+//  Copyright © 2020 Ezequiel Barreto. All rights reserved.
+//
+
+import UIKit
+
+class ImageCellCollectionCell: UICollectionViewCell {
+    private var imageCellView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ImageCellCollectionCell {
+    private func setupViews() {
+        setupImageCell()
+    }
+    
+    private func setupImageCell() {
+        imageCellView.translatesAutoresizingMaskIntoConstraints =  false
+        contentView.addSubview(imageCellView)
+    }
+    
+    private func setupConstraints() {
+        // imageCellView
+        imageCellView.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        imageCellView.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        imageCellView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        imageCellView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        imageCellView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        imageCellView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+    }
+}
--- HelloRxSwift/ImagesCollectionView.swift
+++ HelloRxSwift/ImagesCollectionView.swift
@@ -7,16 +7,51 @@
 //
 
 import UIKit
+import Photos
 
 class ImagesCollectionView: UICollectionViewController {
     
     override init(collectionViewLayout layout: UICollectionViewLayout) {
         super.init(collectionViewLayout: layout)
         setupViews()
+        populatePhotos()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Private
+    private var images = [PHAsset]()
+}
+
+extension ImagesCollectionView {
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+    }
+}
+
+extension ImagesCollectionView {
+    private func populatePhotos() {
+        PHPhotoLibrary.requestAuthorization { [weak self] status in
+            if status == .authorized {
+                let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+                
+                assets.enumerateObjects { (object, count, stop) in
+                    self?.images.append(object)
+                }
+                self?.images.reverse()
+                self?.collectionView.reloadData()
+            }
+        }
     }
 }
 
