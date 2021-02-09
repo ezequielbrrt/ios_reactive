a4484ab44ce8f9ba687269aaf7755a8c7f6a3613
ezequielbrrt <ezequielbrrt@gmail.com>
2021-02-09 17:01:32 -0600

    Filter method added

--- HelloRxSwift/ViewController.swift
+++ HelloRxSwift/ViewController.swift
@@ -41,11 +41,16 @@ class ViewController: UIViewController {
     
     @objc private func applyFilterButtonPressed() {
         guard let sourceImage = self.containerImageView.image else { return }
-        FilterService().applyFilter(to: sourceImage) { filteredImage in
+//        FilterService().applyFilter(to: sourceImage) { filteredImage in
+//            DispatchQueue.main.async { [weak self] in
+//                self?.containerImageView.image = filteredImage
+//            }
+//        }
+        FilterService().applyFilter(to: sourceImage).subscribe(onNext: { filteredImage in
             DispatchQueue.main.async { [weak self] in
                 self?.containerImageView.image = filteredImage
             }
-        }
+        }).disposed(by: disposeBag)
     }
     
     private var disposeBag = DisposeBag()
